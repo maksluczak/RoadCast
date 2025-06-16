2f02eafd128237c712d6e3970351ba3e036b6924
maksluczak <maksluczak99@gmail.com>
2025-06-16 16:39:47 +0200

    app files

---
+++ app/traffic.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
+
+def get_traffic_volume():
+    return 
--- app/weather.py
+++ app/weather.py
@@ -5,11 +5,9 @@ import os
 load_dotenv()
 
 def get_current_weather_by_city(city='Cracow'):
-
     url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("WEATHER_API_KEY")}&q={city}&units=imperial'
-
     data = requests.get(url).json()
-
+    
     return {
         'temperature': data['main']['temp'],
         'rain': data.get('rain', {}).get('1h', 0.0)
