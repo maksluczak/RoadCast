1a4977580c5d51f62dd89f26fb236ca92c87e8d0
maksluczak <maksluczak99@gmail.com>
2025-06-22 21:01:08 +0200

    function exporting database to csv

--- app/database.py
+++ app/database.py
@@ -1,5 +1,9 @@
 import mysql.connector
 from mysql.connector import Error
+import csv
+import os
+
+os.makedirs('./exports', exist_ok=True)
 
 def database_connection():
     try:
@@ -16,19 +20,20 @@ def database_connection():
         print("Error while connecting to database:", e)
         return None, None
 
-def insert_data(weekday: int, hour: int, temperature: float, rain: float, traffic_volume: float, route_lenght:float, trip_duration_minutes:float):
+def insert_data(weekday: int, hour: int, temperature: float, rain: float, traffic_volume: float, route_length:float, trip_duration_minutes:float):
     dbconn, dbcursor = database_connection()
 
     if not dbconn or not dbcursor:
         print("[ERROR] Failed to insert data: no database connection.")
         return
     
-    try:
-        sql = """
-            INSERT INTO traffic_data (weekday, hour, temperature, rain, traffic_volume, route_lenght, trip_duration_minutes) 
+    sql = """
+            INSERT INTO traffic_data (weekday, hour, temperature, rain, traffic_volume, route_length, trip_duration_minutes) 
             VALUES (%s, %s, %s, %s, %s, %s, %s)
             """
-        val = (weekday, hour, temperature, rain, traffic_volume, route_lenght, trip_duration_minutes)
+    val = (weekday, hour, temperature, rain, traffic_volume, route_length, trip_duration_minutes)
+
+    try:
         dbcursor.execute(sql, val)
         dbconn.commit()
         print("Data inserted successfully")
@@ -37,3 +42,33 @@ def insert_data(weekday: int, hour: int, temperature: float, rain: float, traffi
     finally:
         dbcursor.close()
         dbconn.close()
+
+def database_to_csv():
+    dbconn, dbcursor = database_connection()
+
+    if not dbconn or not dbcursor:
+        print("[ERROR] Failed to connect to database.")
+        return
+    
+    sql = "SELECT * FROM traffic_data"
+    csv_file_path = './exports/traffic_data.csv'
+
+    try:
+        dbcursor.execute(sql)
+        rows = dbcursor.fetchall()
+        column_names = [i[0] for i in dbcursor.description]
+    except Error as e:
+        print(f"[ERROR] Failed to retrieve data: {e}")
+        return
+    finally:
+        dbcursor.close()
+        dbconn.close()
+
+    if rows:
+        with open(csv_file_path, 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            csvwriter.writerow(column_names)
+            csvwriter.writerows(rows)
+        print("Data exported to CSV successfully.")
+    else:
+        print("[INFO] No rows found to export.")
