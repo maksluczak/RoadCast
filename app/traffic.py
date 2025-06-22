127b5cfddd739fd1db2fad1e26c4daf6e3dd4c2c
maksluczak <maksluczak99@gmail.com>
2025-06-22 16:55:26 +0200

    function calculating traffic volume

--- app/traffic.py
+++ app/traffic.py
@@ -9,7 +9,7 @@ def get_route_length(start_cords: Tuple, end_cords: Tuple) -> Optional[float]:
     latitude1, longitude1 = start_cords
     latitude2, longitude2 = end_cords
 
-    url = f'https://api.tomtom.com/routing/1/calculateRoute/{latitude1},{longitude1}:{latitude2},{longitude2}/json?traffic=false&key={os.getenv('TOMTOM_API_KEY')}'
+    url = f"https://api.tomtom.com/routing/1/calculateRoute/{latitude1},{longitude1}:{latitude2},{longitude2}/json?traffic=false&key={os.getenv('TOMTOM_API_KEY')}"
     
     try:
         data = requests.get(url).json()
@@ -20,8 +20,32 @@ def get_route_length(start_cords: Tuple, end_cords: Tuple) -> Optional[float]:
         print(f"[ERROR] Error while retrieving route: {e}")
         return None
     
-def get_traffic_volume(start_cords: Tuple, end_cords: Tuple, points: int = 5) -> Optional[float]:
-    pass
+def get_traffic_volume(start_cords: Tuple, end_cords: Tuple) -> Optional[float]:
+    points = generate_route_points(start_cords, end_cords, 5)
+    traffic_volume = []
+
+    for lat, lon in points:
+        url = f"https://api.tomtom.com/traffic/services/4/flowSegmentData/absolute/10/json?point={lat},{lon}&unit=KMPH&key={os.getenv('TOMTOM_API_KEY')}"
+
+        try:
+            resp = requests.get(url)
+            resp.raise_for_status()
+            data = resp.json()
+
+            segment = data.get('flowSegmentData', {})
+            currentSpeed = segment.get('currentSpeed')
+            freeFlowSpeed = segment.get('freeFlowSpeed')
+
+            if currentSpeed and freeFlowSpeed:
+                vol = 1 - (currentSpeed / freeFlowSpeed)
+                traffic_volume.append(vol)
+        except Exception as e:
+            print(f"[ERROR] Error while retrieving route: {e}")
+        
+    if traffic_volume:
+        return sum(traffic_volume) / len(traffic_volume)
+    else:
+        return None
 
 def generate_route_points(start_cords: Tuple, end_cords: Tuple, n: int) -> List[Tuple[float]]:
     latitude1, longitude1 = start_cords
@@ -29,9 +53,8 @@ def generate_route_points(start_cords: Tuple, end_cords: Tuple, n: int) -> List[
 
     return [
         (
-            (latitude1 + (latitude2 - latitude2) * i) / (n - 1),
-            (longitude1 + (longitude2 - longitude2) * i) / (n - 1)
+            latitude1 + (latitude2 - latitude1) * i / (n - 1),
+            longitude1 + (longitude2 - longitude1) * i / (n - 1)
         )
         for i in range(n)
     ]
-
