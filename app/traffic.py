c33ea6c24b8dadbcc6cbc787e99d50359bdf9787
maksluczak <maksluczak99@gmail.com>
2025-06-22 16:02:28 +0200

    route length function

--- app/traffic.py
+++ app/traffic.py
@@ -1,8 +1,21 @@
 from dotenv import load_dotenv
+from typing import Tuple, List
 import requests
 import os
 
 load_dotenv()
 
-def get_traffic_volume():
-    return 
+def get_route_length(start_cords: Tuple, end_cords: Tuple) -> float:
+    latitude1, longitude1 = start_cords
+    latitude2, longitude2 = end_cords
+
+    url = f'https://api.tomtom.com/routing/1/calculateRoute/{latitude1},{longitude1}:{latitude2},{longitude2}/json?traffic=false&key={os.getenv('TOMTOM_API_KEY')}'
+    
+    try:
+        data = requests.get(url).json()
+
+        route_length = data['routes'][0]['summary']['lengthInMeters'] / 1000
+        return route_length
+    except Exception as e:
+        print(f"[ERROR] Error while retrieving route: {e}")
+        return None
