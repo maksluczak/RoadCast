4af1a311f67b1b12c637e44fa52971bc8f6bdd0c
maksluczak <maksluczak99@gmail.com>
2025-06-22 16:33:03 +0200

    function generating route points, interpolating actual road

--- app/traffic.py
+++ app/traffic.py
@@ -1,11 +1,11 @@
 from dotenv import load_dotenv
-from typing import Tuple, List
+from typing import Tuple, List, Optional
 import requests
 import os
 
 load_dotenv()
 
-def get_route_length(start_cords: Tuple, end_cords: Tuple) -> float:
+def get_route_length(start_cords: Tuple, end_cords: Tuple) -> Optional[float]:
     latitude1, longitude1 = start_cords
     latitude2, longitude2 = end_cords
 
@@ -19,3 +19,19 @@ def get_route_length(start_cords: Tuple, end_cords: Tuple) -> float:
     except Exception as e:
         print(f"[ERROR] Error while retrieving route: {e}")
         return None
+    
+def get_traffic_volume(start_cords: Tuple, end_cords: Tuple, points: int = 5) -> Optional[float]:
+    pass
+
+def generate_route_points(start_cords: Tuple, end_cords: Tuple, n: int) -> List[Tuple[float]]:
+    latitude1, longitude1 = start_cords
+    latitude2, longitude2 = end_cords
+
+    return [
+        (
+            (latitude1 + (latitude2 - latitude2) * i) / (n - 1),
+            (longitude1 + (longitude2 - longitude2) * i) / (n - 1)
+        )
+        for i in range(n)
+    ]
+
