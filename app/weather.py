e75e39e1202377afa5c0020366dc1ae83265f253
maksluczak <maksluczak99@gmail.com>
2025-06-22 20:24:58 +0200

    change fahrenheit to celsius

--- app/weather.py
+++ app/weather.py
@@ -5,7 +5,7 @@ import os
 load_dotenv()
 
 def get_current_weather_by_city(city: str):
-    url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("WEATHER_API_KEY")}&q={city}&units=imperial'
+    url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("WEATHER_API_KEY")}&q={city}&units=metric'
     try:
         data = requests.get(url).json()
 
