2cd680be5e1cd680086d4eff4a8a4a044145e45d
maksluczak <maksluczak99@gmail.com>
2025-06-22 17:29:09 +0200

    function inserting data into database

--- app/database.py
+++ app/database.py
@@ -3,15 +3,37 @@ from mysql.connector import Error
 
 def database_connection():
     try:
-        mydb = mysql.connector.connect(
+        conn = mysql.connector.connect(
             host="localhost",
             user="root",
             password="",
             database="roadcast_db"
         )
-        if mydb.is_connected():
-            cursor = mydb.cursor()
+        if conn.is_connected():
             print("Database connected successfully")
-        
+            return conn, conn.cursor()
     except Error as e:
-        print("Error while connecting to database: " + e)
+        print("Error while connecting to database:", e)
+        return None, None
+
+def insert_data(weekday: int, hour: int, temperature: float, rain: float, traffic_volume: float, trip_duration_minutes:float, historical_travel_time:float):
+    dbconn, dbcursor = database_connection()
+
+    if not dbconn or not dbcursor:
+        print("[ERROR] Failed to insert data: no database connection.")
+        return
+    
+    try:
+        sql = """
+            INSERT INTO traffic_data (weekday, hour, temperature, rain, traffic_volume, trip_duration_minutes, historical_travel_time) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+            """
+        val = (weekday, hour, temperature, rain, traffic_volume, trip_duration_minutes, historical_travel_time)
+        dbcursor.execute(sql, val)
+        dbconn.commit()
+        print("Data inserted successfully")
+    except Error as e:
+        print(f"[ERROR] Failed to insert data: {e}")
+    finally:
+        dbcursor.close()
+        dbconn.close()
